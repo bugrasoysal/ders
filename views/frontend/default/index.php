b58042e55cb72d18267b54744126e05783268a32
bugrasoysal <150202010@kocaeli.edu.tr>
2017-12-29 15:34:56 +0300

    update

--- views/frontend/default/index.php
+++ views/frontend/default/index.php
@@ -3,9 +3,8 @@
 $this->title = 'WYP Ders Application';
 ?>
 
-
-<!-- Codes by HTML.am -->
-
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular.min.js"></script>
+<script type="text/javascript" src="js/jquery-1.7.1.min.js"></script>
 <!-- CSS Code -->
 <style type="text/css" scoped>
 table.GeneratedTable {
@@ -16,21 +15,22 @@ border-color:#000000;
 border-style:solid;
 color:#000000;
 }
-
 table.GeneratedTable td, table.GeneratedTable th {
 border-width:1px;
 border-color:#000000;
 border-style:solid;
 padding:6px;
 }
-
 table.GeneratedTable thead {
 background-color:#6666FF;
 }
 </style>
 
 <!-- HTML Code -->
-<table class="GeneratedTable">
+
+
+<br>
+<table class="GeneratedTable" id="myTable">
 <thead>
 <tr>
 <th>Dersler</th>
@@ -53,7 +53,29 @@ background-color:#6666FF;
 </tbody>
 </table>
 <br>
- Ders Ekle:
- <input type="text" name="ders">
- <input type="submit" value="Ekle">
- 
+<div 
+<p>Dersler: <input type="text" id="ders"></p>
+
+</div>
+<input type="button" value="Ekle" onClick="addRow()" border=0       style='cursor:hand'>
+<input type="button" value="Sil" onClick='deleteRow()' border=0    style='cursor:hand'>
+
+
+
+<SCRIPT language="javascript">
+
+
+var ders=$scope.name;
+function addRow(){
+ var table = document.getElementById("myTable");
+ var row = table.insertRow(-1);
+ var cell1 = row.insertCell(-1);
+ cell1.innerHTML =ders ;
+}
+
+function deleteRow() {
+    document.getElementById("myTable").deleteRow(-1);
+
+}
+
+</script>
